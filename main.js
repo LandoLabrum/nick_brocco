d77bfebb10d62145f8786385f876a922a78d2d52
landolabrum <lando@deepturn.com>
2021-10-07 03:40:19 -0600

    Updates

--- main.js
+++ main.js
@@ -13,12 +13,18 @@ function createActive() {
 document.addEventListener('click', function (event) {
     if (!event.target.matches('.event')) return;
     event.preventDefault();
-    var par = event.target.parentNode.parentNode
-    if(par.firstChild.classList.contains("material-icons")){
-        par.removeChild(par.childNodes[0]);
-    }else{
-        par.insertBefore(createActive(), par.firstChild);
-    }
+    var par = event.target.parentNode
+    console.log(par.classList.contains("green"))
+    // if(!par.classList.contains("green")){
+        // par.removeChild(par.childNodes[0]);
+    //     par.classList.toggle("box-content-color");
+    //     par.childNodes[1].classList.toggle("white-text");
+    // }else{
+        // par.classList.toggle("gray lighten-3");
+        // par.insertBefore(createActive(), par.firstChild);
+    // }
+    par.classList.toggle("box-content-color");
+    par.classList.toggle("white-text");
     
 }, false);
 
